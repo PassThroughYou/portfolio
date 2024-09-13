26a5f65d372b25de5ea6d53034c4f22c2e3e4413
stargnastar <wlskb@naver.com>
2024-09-13 20:03:29 +0900

    [Func] 상단 카드 모듈

--- src/App.js
+++ src/App.js
@@ -26,10 +26,16 @@ function App() {
       <div className="Wrapper">
 
 
-        <div className='item-list'>
+        <div className='itemList'>
           <ul className='card'>
-           
+           <li>
+            <h1>JiNa Yu</h1>
+            <p>본질적인 문제에 집중하며, '왜?'에 대한 해답을 찾아갑니다.</p>
+            <p className="cardResume">이력서</p>
+           </li>
           </ul>
+
+          <img className='cardImage' alt="cardImage" src="image/JinaLogo.png" />
         </div>
 
       </div>
